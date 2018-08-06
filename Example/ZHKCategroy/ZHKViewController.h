0be0550e1815caa263830ec19ce9fd4b84d3f65c
ZHK1024 <389914070@qq.com>
2018-08-06 15:34:50 +0800

    备份

---
+++ Example/ZHKCategroy/ZHKViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZHKViewController.h
+//  ZHKCategroy
+//
+//  Created by 389914070@qq.com on 08/06/2018.
+//  Copyright (c) 2018 389914070@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZHKViewController : UIViewController
+
+@end
---
+++ Example/ZHKCategroy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ZHKCategroy
+//
+//  Created by 389914070@qq.com on 08/06/2018.
+//  Copyright (c) 2018 389914070@qq.com. All rights reserved.
+//
+
+@import UIKit;
+#import "ZHKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZHKAppDelegate class]));
+    }
+}
