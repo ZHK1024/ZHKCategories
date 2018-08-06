ba2574bcb09db2983c4934c97729a15aef6ef42a
ZHK1024 <389914070@qq.com>
2018-08-06 15:34:21 +0800

    A

--- Example/Pods/Target Support Files/Pods-ZHKCategroy_Tests/Pods-ZHKCategroy_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZHKCategroy_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ZHKCategroy_Tests
-@end
--- Example/Pods/Target Support Files/ZHKCategroy/ZHKCategroy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZHKCategroy : NSObject
-@end
-@implementation PodsDummy_ZHKCategroy
-@end
