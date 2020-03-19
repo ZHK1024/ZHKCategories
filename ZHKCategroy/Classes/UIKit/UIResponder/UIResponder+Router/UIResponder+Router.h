f8c992d4411dd2e94611fb21ddfdbee849d62c8c
TongJunfeng <389914070@qq.com>
2020-03-19 11:09:48 +0800

    新增 UIResponder 扩展

--- ZHKCategroy/Classes/UIKit/UIResponder/UIResponder+Router/UIResponder+Router.h
+++ ZHKCategroy/Classes/UIKit/UIResponder/UIResponder+Router/UIResponder+Router.h
@@ -10,6 +10,24 @@
 
 @interface UIResponder (Router)
 
+///  利用响应链原理传递字典
+/// @param eventName 事件名称
+/// @param userInfo  传输数据字典
 - (void)routerEventName:(NSString *)eventName userInfo:(NSDictionary *)userInfo;
+
+///  利用响应链原理传递字典
+/// @param eventTag  事件标识
+/// @param userInfo  传输数据字典
+- (void)routerEventTag:(NSInteger)eventTag userInfo:(NSDictionary *)userInfo;
+
+/// 利用响应链传递对象
+/// @param eventName 事件名称
+/// @param object    对象
+- (void)routerEventName:(NSString *)eventName object:(NSObject *)object;
+
+/// 利用响应链传递对象
+/// @param eventTag  事件标识
+/// @param object    对象
+- (void)routerEventTag:(NSInteger)eventTag object:(NSObject *)object;
 
 @end
--- ZHKCategroy/Classes/UIKit/UIResponder/UIResponder+Router/UIResponder+Router.m
+++ ZHKCategroy/Classes/UIKit/UIResponder/UIResponder+Router/UIResponder+Router.m
@@ -14,4 +14,25 @@
     [[self nextResponder] routerEventName:eventName userInfo:userInfo];
 }
 
+///  利用响应链原理传递字典
+/// @param eventTag  事件标识
+/// @param userInfo  传输数据字典
+- (void)routerEventTag:(NSInteger)eventTag userInfo:(NSDictionary *)userInfo {
+    [[self nextResponder] routerEventTag:eventTag userInfo:userInfo];
+}
+
+/// 利用响应链传递对象
+/// @param eventName 事件名称
+/// @param object    对象
+- (void)routerEventName:(NSString *)eventName object:(NSObject *)object {
+    [[self nextResponder] routerEventName:eventName object:object];
+}
+
+/// 利用响应链传递对象
+/// @param eventTag  事件标识
+/// @param object    对象
+- (void)routerEventTag:(NSInteger)eventTag object:(NSObject *)object {
+    [[self nextResponder] routerEventTag:eventTag object:object];
+}
+
 @end
